eafae37d61d341cbbbbd08fb3b0e144679b5af0b
captkark <captainkark@gmail.com>
2026-01-05 22:47:03 +0600

    W1D2 - Implement Database Persistence with PostgreSQL and ORM

---
+++ database.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# 1. Database Connection URL
+# CHANGE 'YOUR_PASSWORD' to the password you set for the postgres user
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost:5432/docusafe_db"
+
+# 2. Database Engine
+# This handles the actual communication to the DB
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+
+# 3. Session Factory
+# This is what we use to create a session (a temporary workspace) to talk to the DB
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# 4. Declarative Base
+# This is the base class for all your DB models/tables
+Base = declarative_base()
