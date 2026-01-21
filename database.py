d4fe8188dfe3c11a1da5df0d48db25d334313f98
captkark <captainkark@gmail.com>
2026-01-21 22:09:05 +0600

    Refactor: Restored Standard DB port 5432 after CleanUp

--- database.py
+++ database.py
@@ -4,7 +4,8 @@ from sqlalchemy.orm import sessionmaker
 
 # 1. Database Connection URL
 # CHANGE 'YOUR_PASSWORD' to the password you set for the postgres user
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost:5433/docusafe_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost:5432/docusafe_db"
+
 # 2. Database Engine
 # This handles the actual communication to the DB
 engine = create_engine(
