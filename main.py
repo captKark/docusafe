eea36c603cb357184c0aa670256d3513752e8d43
captkark <captainkark@gmail.com>
2026-01-26 21:03:46 +0600

    W2D5: Project CleanUp and added requirements.txt for deployment

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import models
 from database import engine
 from routers import users, auth, documents
 
-# 1. The Architect: Create tables
-models.Base.metadata.create_all(bind=engine)
 
+# CREATE THE FASTAPI INSTANCE / THE APP
 app = FastAPI()
 
-# 2. Wiring: Connect the Router
+# DATABASE SETUP: AUTO-CREATE TABLES 
+models.Base.metadata.create_all(bind=engine)
+
+# CORS (Optional, but good for frontend-backend communication)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow any Frontend to talk to us/  Adjust as needed for security
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# CONNECT ROUTERs
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(documents.router)
 
 @app.get("/")
 async def root():
-    return {"message": "Docusafe API is running"}
+    return {"message": "Welcome to the DocuSafe API"}
